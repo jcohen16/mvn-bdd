339d967ce063e31fe5ecd76185e3dbde62697274
jctest-at-425152363247 <jack.cohen@yahoo.com>
2018-06-09 13:53:03 -0400

    Added log4j

--- src/test/java/test/Steps.java
+++ src/test/java/test/Steps.java
@@ -1,11 +1,19 @@
 package test;
 
 import cucumber.api.java.en.Given;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class Steps {
+
+    Logger systemLogger = LogManager.getLogger("systemLogger");
+    Logger resultsLogger = LogManager.getLogger("resultsLogger");
 
     @Given("^I run a test$")
     public void test(){
         System.out.println("test");
+        systemLogger.info("system log info message");
+        resultsLogger.info("results log info message");
+        System.out.println();
     }
 }
